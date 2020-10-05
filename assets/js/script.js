4c283141e15b231b66aa087c39733560529bf845
khuro <gaete.gaona@gmail.com>
2020-10-04 21:19:37 -0300

    cambios en el tamaño del form

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,6 @@ $(".jumbotron").css({ height: $(window).height() + "px" });
 $(window).on("resize", function() {
     $(".jumbotron").css({ height: $(window).height() + "px" });
 });
+$(function() {
+    $('[data-toggle="tooltip"]').tooltip()
+})
