01e30b854ff0f9354d7007dfcc3ebbc90c437415
khuro <gaete.gaona@gmail.com>
2021-05-06 22:30:03 -0400

    nevos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,24 @@
+$('.carousel').carousel({
+    interval: 200
+})
+
 $(".jumbotron").css({ height: $(window).height() + "px" });
 
 $(window).on("resize", function() {
     $(".jumbotron").css({ height: $(window).height() + "px" });
 });
+$(function() {
+    $('[data-toggle="tooltip"]').tooltip()
+})
+
+$('#myTab a').on('click', function(e) {
+    e.preventDefault()
+    $(this).tab('show')
+})
+
+$('#someTab').tab('show')
+
+$('a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
+    e.target // newly activated tab
+    e.relatedTarget // previous active tab
+})
