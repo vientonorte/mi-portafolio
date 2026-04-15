3305420feb4852a8be482b0954e88fe7702172d4
vientonorte <56666970+vientonorte@users.noreply.github.com>
2026-04-15 17:29:28 -0400

    fix: eliminar duplicado App.tsx, import AutosuggestFondos, meta tags completos

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,10 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import Proyectos from './pages/Proyectos';
+import AutosuggestFondos from './pages/AutosuggestFondos';
 import SobreMi from './pages/SobreMi';
 import Contacto from './pages/Contacto';
 import Privacy from './pages/Privacy';
-
 import Grafo from './pages/Grafo';
 
 const App = () => (
@@ -29,32 +29,3 @@ const App = () => (
 );
 
 export default App;
-        import React from 'react';
-        import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-        import Header from './components/Header';
-        import Footer from './components/Footer';
-        import Home from './pages/Home';
-        import Proyectos from './pages/Proyectos';
-        import SobreMi from './pages/SobreMi';
-        import Contacto from './pages/Contacto';
-        import Privacy from './pages/Privacy';
-        import AutosuggestFondos from './pages/AutosuggestFondos';
-
-        const App = () => (
-          <Router>
-            <Header />
-            <main id="main" tabIndex={-1}>
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/proyectos" element={<Proyectos />} />
-                <Route path="/proyectos/autosuggest-fondos" element={<AutosuggestFondos />} />
-                <Route path="/sobre-mi" element={<SobreMi />} />
-                <Route path="/contacto" element={<Contacto />} />
-                <Route path="/privacy" element={<Privacy />} />
-              </Routes>
-            </main>
-            <Footer />
-          </Router>
-        );
-
-        export default App;
